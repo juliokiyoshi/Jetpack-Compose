26ec1895c11c8a847c13c5889cded0ba6d30d30a
Julio Matsoui <julio.matsoui@eldorado.org.br>
2023-04-10 14:24:05 -0300

    leasson: Configure layout

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
-import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,9 +36,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column{
-        Text(text = "MY FIRST APP IN COMPOSE $name!")
-        Text(text= "novoSASASA app")
+    Row(modifier = Modifier.padding(all = 10.dp)) {
+        Image(
+            painter = painterResource(R.drawable.vector),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(30.dp)
+                .clip(CircleShape)
+        )
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Column {
+            Text(text = "MY FIRST APP IN COMPOSE $name!")
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "meu novo app")
+        }
+
     }
 }
 
