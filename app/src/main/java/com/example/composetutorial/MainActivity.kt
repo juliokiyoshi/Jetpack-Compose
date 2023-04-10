8c3135768f5e1614af7f5981abdb741008dc5c4e
Julio Matsoui <julio.matsoui@eldorado.org.br>
2023-04-10 14:47:16 -0300

    Leasson: Material Desing

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
@@ -43,21 +47,43 @@ fun Greeting(name: String) {
             modifier = Modifier
                 .size(30.dp)
                 .clip(CircleShape)
+                .border(1.dp, MaterialTheme.colors.secondaryVariant, CircleShape)
         )
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(10.dp))
 
         Column {
-            Text(text = "MY FIRST APP IN COMPOSE $name!")
+            Text(
+                text = "MY FIRST APP IN COMPOSE $name!",
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(10.dp))
-            Text(text = "meu novo app")
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+
+                    text = "meu novo app com Compose",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(horizontal = 10.dp),
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.body2
+
+                )
+            }
         }
 
     }
 }
 
 @Preview(showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "dark Mode"
+)
 @Composable
 fun DefaultPreview() {
     ComposeTutorialTheme {
