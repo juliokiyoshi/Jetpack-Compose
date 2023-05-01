a11ebea5eae7f64d3471e7b933af83cc7e87def2
Julio Matsoui <julio.matsoui@eldorado.org.br>
2023-04-30 21:37:19 -0300

    added mutable state list to be observable

--- app/src/main/java/com/example/composetutorial/WellnessScreen.kt
+++ app/src/main/java/com/example/composetutorial/WellnessScreen.kt
@@ -1,9 +1,19 @@
 package com.example.composetutorial
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
+    }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
